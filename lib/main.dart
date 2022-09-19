60cac06a6a47fcbe14d5ba9130591d1dd27b3ce2
Denis <jaddev888@gmail.com>
2022-09-19 21:08:23 +0300

    complete simple chat

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:simple_chat/pages/splash_page.dart';
+import 'package:simple_chat/utils/constants.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    // TODO: Replace credentials with your own
+    url: 'SUPABASE_URL',
+    anonKey: 'SUPABASE_ANON_KEY',
+  );
+
   runApp(const MyApp());
 }
-
+//https://supabase.com/blog/flutter-tutorial-building-a-chat-app
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Chat',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
+      debugShowCheckedModeBanner: false,
+      title: 'My Chat App',
+      theme: appTheme,
+      home: const SplashPage(),
     );
   }
 }
